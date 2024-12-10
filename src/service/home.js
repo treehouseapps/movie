784f6c760e358d708d2c9da5de3a1fa59cac86b5
zelalem888 <102322904+zelalem888@users.noreply.github.com>
2024-12-10 23:31:03 +0300

    search movies/tv only home page

--- src/service/home.js
+++ src/service/home.js
@@ -1,61 +1,67 @@
 import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query"
-import axios from 'axios'
-import { Button } from '@mui/material';
-import { lazy, Suspense } from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Button } from "@mui/material";
+import { lazy, Suspense } from "react";
 
 import Navbar from "../components/navbar";
 import Slide from "../components/Slide";
 import Card from "../components/card/card";
 import LoadingSpin from "../components/ui/loadingSpin";
 
-import api from '../api/axios'
-import useFetch from '../hooks/useFetch'
+import api from "../api/axios";
+import useFetch from "../hooks/useFetch";
 
 const Home = () => {
   // const url = "https://api.themoviedb.org/3/discover/movie?api_key=777a620fe0481ab7da037455f07f283b";
   const url = "/discover/movie";
 
   const fetchMovies = async (url, page = {}) => {
-    const moviesResponse = await api.get("/discover/movie", { params: { page } })
-    const tvResponse = await api.get("/trending/tv/week", { params: { page } })
+    const moviesResponse = await api.get("/discover/movie", {
+      params: { page },
+    });
+    const tvResponse = await api.get("/trending/tv/week", { params: { page } });
 
-    const movies = moviesResponse.data.results.slice(0, 10)
-    const tv = tvResponse.data.results
+    const movies = moviesResponse.data.results.slice(0, 10);
+    const tv = tvResponse.data.results;
+    console.log(movies);
 
+    return { movies, tv };
+    console.log(movies);
+    
+  };
 
-    return { movies, tv }
-  }
   
+
   const { data, isLoading, error } = useQuery({
-      queryKey: ["movies"], 
-      queryFn: () => fetchMovies(url), 
-      refetchInterval: 90000
-    })
+    queryKey: ["movies"],
+    queryFn: () => fetchMovies(url),
+    refetchInterval: 90000,
+  });
 
   return (
     <div className="container-fluid body-container">
-    { isLoading && <LoadingSpin data={ isLoading } /> }
-    { error && <h2 className="error"> { error.message } </h2> }
-    { (data?.movies?.length > 0) && 
-      <>
-      <div className="header">
-        
-        <Slide data={data.movies?.slice(0, 3)} />
-      </div>
-      
-      <div className="container-fluid contents-container">
-        <div className="recommended">
-          <div className="catagory">
-            <p>Trending</p>
-            <button className="btn btn-primary">Movies</button>
-            <button className="btn btn-outline-primary">TV Shows</button>
+      {isLoading && <LoadingSpin data={isLoading} />}
+      {error && <h2 className="error"> {error.message} </h2>}
+      {data?.movies?.length > 0 && (
+        <>
+          <div className="header">
+            <Slide data={data.movies?.slice(0, 3)} />
           </div>
-          <Card data={data.movies} title="Popular Movies" />
-          <Card data={data.tv} title="Trending TV Shows" />
-        </div>
-      </div>
-      </> }
+
+          <div className="container-fluid contents-container">
+            <div className="recommended">
+              <div className="catagory">
+                <p>Trending</p>
+                <button className="btn btn-primary">Movies</button>
+                <button className="btn btn-outline-primary">TV Shows</button>
+              </div>
+              <Card data={data.movies} title="Popular Movies" />
+              <Card data={data.tv} title="Trending TV Shows" />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
