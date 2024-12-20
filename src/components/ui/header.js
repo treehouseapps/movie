64d9467ec88438b2252ab3626e80c85ae623b9f9
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-12-20 21:35:23 +0300

    update on header design in movies page

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -10,82 +10,93 @@ import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 const Title = () => {
     return (
-        <Box
-            sx={{
-                backgroundColor: 'white',
-                margin: '0.5rem 1rem',
-                marginTop: '2rem',
-                padding: '10px',
-                borderRadius: '30px'
-            }}
-        >
-            <Typography variant='h4' align='center' sx={{ margin: '.5rem', borderRadius: '1rem' }}>
-                <p className="section-title"><Typography variant='h5'>Watch Movies, TV shows and more</Typography></p>
-            </Typography>
-            <Grid container justifyContent="space-between">
-                <Grid item>
-                    <Typography variant="h4" sx={{ marginLeft: '5rem' }}>
-                        <DoubleArrowIcon />    Movies
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Paper
-                        component="form"
+        <div className='header'>
+            <div className="header-search">
+                <Paper
+                    component="form"
+                    sx={{
+                        p: '2px 4px',
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        width: 400,
+                        boxShadow: 'none',
+                    }}
+                >
+                    <TextField
+                        variant="outlined"
+                        placeholder="Search ..."
                         sx={{
-                            p: '2px 4px',
-                            display: 'flex',
-                            alignItems: 'center',
-                            width: 400,
-                            backgroundColor: 'transparent',
-                            boxShadow: 'none',
-                            border: 'none',
-                            marginRight: "2rem"
+                            ml: 'auto',
+                            width: '50px',
+                            transition: 'width 0.3s ease-in-out',
+                            '& .MuiOutlinedInput-root': {
+                                paddingRight: '40px',
+                                '& fieldset': {
+                                    border: 'none',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    border: 'none',
+                                },
+                            },
+                            '&:focus-within': {
+                                width: '200px',
+                                border: '1px solid black',
+                                borderRadius: '1rem',
+                            },
                         }}
+                        InputProps={{
+                            endAdornment: (
+                                <IconButton
+                                    type="button"
+                                    sx={{
+                                        p: '10px',
+                                        color: 'black',
+                                        position: 'absolute',
+                                        right: '10px',
+                                    }}
+                                    aria-label="search"
+                                    onClick={() => console.log('Search clicked!')}
+                                >
+                                    <SearchIcon sx={{ color: 'black', fontSize: '30px' }} />
+                                </IconButton>
+                            ),
+                        }}
+                    />
+                </Paper>
+            </div>
+            <Box
+                sx={{
+                    backgroundColor: 'white',
+                    margin: '0.5rem 1rem',
+                    marginTop: '0rem',
+                    padding: '10px',
+                    paddingTop: '0px',
+                    borderRadius: '30px'
+                }}
+            >
+                <Typography variant='h4' align='center' sx={{ margin: '.5rem', marginTop: '0px', borderRadius: '1rem' }}>
+                    <Typography
+                        variant="h5"
+                        className="section-title"
+                        sx={{ paddingTop: '0px' }}
                     >
-                        <TextField
-                            variant="outlined"
-                            placeholder="Search ..."
-                            fullWidth
-                            sx={{
-                                ml: 'auto',
-                                width: '50px',
-                                transition: 'width 0.3s ease-in-out',
-                                '& .MuiOutlinedInput-root': {
-                                    paddingRight: '40px',
-                                    '& fieldset': {
-                                        border: 'none',
-                                    },
-                                    '&.Mui-focused fieldset': {
-                                        border: 'none',
-                                    },
-                                },
-                                '&:focus-within': {
-                                    width: '200px',
-                                    border: '1px solid black',
-                                    borderRadius: '1rem',
-                                },
-                            }}
-                            InputProps={{
-                                endAdornment: (
-                                    <IconButton
-                                        type="button"
-                                        sx={{ p: '10px', color: 'black', position: 'absolute', right: '10px' }} // Positioning icon
-                                        aria-label="search"
-                                        onClick={() => console.log('Search clicked!')}
-                                    >
-                                        <SearchIcon sx={{ color: 'black', fontSize: '30px' }} />
-                                    </IconButton>
-                                ),
-                            }}
-                        />
-                    </Paper>
+                        Watch Movies, TV shows, and more
+                    </Typography>
 
+                </Typography>
+                <Grid container justifyContent="space-between">
+                    <Grid item>
+                        <Typography variant="h4" sx={{}}>
+                            <DoubleArrowIcon />    Movies
+                        </Typography>
+                    </Grid>
+                    <Grid item>
 
+                    </Grid>
 
-                </Grid>
-
-            </Grid >
-        </Box >
+                </Grid >
+            </Box >
+        </div>
     );
 };
 
